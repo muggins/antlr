be43ff96c8a92eb1fe1321688ac9fa0a6fd6f6ad
acondit <acondit@parrt.spork>
2011-01-10 20:26:03 -0800

    Fixes to lookahead and backtracking
    [git-p4: depot-paths = "//depot/code/antlr/main/": change = 7410]

--- runtime/ObjC/Framework/ANTLRLookaheadStream.h
+++ runtime/ObjC/Framework/ANTLRLookaheadStream.h
@@ -36,11 +36,12 @@
 
 @interface ANTLRLookaheadStream : ANTLRFastQueue
 {
-	id eof;
     NSInteger currentElementIndex;
 	NSInteger eofElementIndex;
 	NSInteger lastMarker;
 	NSInteger markDepth;
+	id prevElement;
+	id eof;
 }
 
 @property (readwrite, retain, getter=getEof, setter=setEof:) id eof;
@@ -48,9 +49,11 @@
 @property (assign, getter=getEofElementIndex, setter=setEofElementIndex:) NSInteger eofElementIndex;
 @property (assign, getter=getLastMarker, setter=setLastMarker:) NSInteger lastMarker;
 @property (assign, getter=getMarkDepth, setter=setMarkDepth:) NSInteger markDepth;
+@property (assign, getter=getPrevElement, setter=setPrevElement:) id prevElement;
 
 - (id) initWithEOF:(id) o;
 - (id) nextElement;
+- (id) remove;
 - (void) consume;
 - (void) sync:(NSInteger) need;
 - (void) fill:(NSInteger) n;
